46edc918ff58e8e0396624ad03445fe47524e355
liquan1que1@gmail.com <liquan1que1@gmail.com>
2019-12-21 16:27:14 +1100

    binarytree

---
+++ chapter3/balancedBinaryTree.cpp
@@ -0,0 +1,161 @@
+/**
+QUESTION:
+110. Balanced Binary Tree
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node 
+differ in height by no more than 1.
+
+Example 1:
+
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+Example 2:
+
+Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+Return false.
+ */
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+
+    struct returnType{
+        bool balanced;
+        int height;
+    };
+    
+public:
+    bool isBalanced(TreeNode* root) {
+        auto result = isBalancedAux(root);
+        return result.balanced;
+    }
+    
+    returnType isBalancedAux(TreeNode* root) {
+        returnType result;
+        if(root == nullptr){
+            result.height = 0;
+            result.balanced = true;
+            return result;
+        }
+        
+        auto left = isBalancedAux(root->left);
+        auto right = isBalancedAux(root->right);
+        
+        if(left.balanced  && 
+           right.balanced &&
+           abs(left.height - right.height) <= 1){
+            result.balanced = true;
+        }
+        else{
+            result.balanced = false;
+        }
+        
+        result.height =  max(left.height, right.height) + 1;
+        
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        bool ret = Solution().isBalanced(root);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
---
+++ chapter3/binarySearchTreeIterator.cpp
@@ -0,0 +1,98 @@
+/**
+QUESTION:
+173. Binary Search Tree Iterator
+
+
+
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+ 
+
+Example:
+
+
+
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // return 3
+iterator.next();    // return 7
+iterator.hasNext(); // return true
+iterator.next();    // return 9
+iterator.hasNext(); // return true
+iterator.next();    // return 15
+iterator.hasNext(); // return true
+iterator.next();    // return 20
+iterator.hasNext(); // return false
+ 
+
+Note:
+
+next() and hasNext() should run in average O(1) time and uses O(h) memory, 
+where h is the height of the tree.
+You may assume that next() call will always be valid, that is, 
+there will be at least a next smallest number in the BST when next() is called.
+
+*/
+
+/**
+SOLUTION:
+ stimulate the iteratively in-order traversal process
+
+
+ */
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    
+    
+    BSTIterator(TreeNode* root) {
+        currentNode = root;
+        while(currentNode != nullptr){
+            nodeStack.push(currentNode);
+            currentNode = currentNode->left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        auto result = nodeStack.top();
+        nodeStack.pop();
+        currentNode = result->right;
+        while(currentNode != nullptr){
+            nodeStack.push(currentNode);
+            currentNode = currentNode->left;
+        }
+        
+        return result->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !nodeStack.empty();
+    }
+    
+private:
+    stack<TreeNode*> nodeStack;
+    TreeNode* currentNode;
+    
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
